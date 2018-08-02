cd2e0ffc696e0f6e2e550f65dd67b993cf420ef3
GeneralOy <League@AdministorsiMac.attlocal.net>
2018-08-02 16:59:59 -0700

    Finished stuffs
    Continue at #2 - Pixel Art

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,27 +1,33 @@
 package _01_Crazy_Digital_Painting;
 
-public class CrazyDigitalPainting {
-	//1. Create two final static integers for the width and height of the display.
+import java.awt.Color;
 
+public class CrazyDigitalPainting {
+	/**1. Create two final static integers for the width and height of the display.*/
+	static final int width = 500, height = 500;
 	
-	//2. Create a 2D array of Color objects. You will need to import
+	/**2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
-	//integers created in step 1.
-	
+	//integers created in step 1.*/
+	Color[][] colorArray = new Color[width][height];
 	
 	
 	public CrazyDigitalPainting() {
-		//3. Open the crazy_digital_painting.png file and look at the image.
-		
-		//4. Iterated through the 2D array and initialize each Color object
+		/**3. Open the crazy_digital_painting.png file and look at the image.*/
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
+				colorArray[i][j] = new Color((i) % 256, (8*i + 5*j) % 256, (j) % 256);
+			}
+		}
+		/**4. Iterate through the 2D array and initialize each Color object
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
-		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);*/
 		
-		//5. Come up with your own pattern to make a cool crazy image.
-		
-		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
-		//   to show off your picture.
+		/**5. Come up with your own pattern to make a cool crazy image.*/
+		ColorArrayDisplayer.displayColorsAsImage(colorArray);
+		/**6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
+		//   to show off your picture.*/
 	}
 	
 	public static void main(String[] args) {
--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -16,7 +16,8 @@ public class GridPanel extends JPanel{
 	private int rows;
 	private int cols;
 	
-	//1. Create a 2D array of pixels. Do not initialize it yet.
+	/**1. Create a 2D array of pixels. Do not initialize it yet.*/
+	Pixel[][] pixelArray;
 	
 	private Color color;
 	
@@ -33,11 +34,15 @@ public class GridPanel extends JPanel{
 		
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
 		
-		//2. Initialize the pixel array using the rows and cols variables.
+		/**2. Initialize the pixel array using the rows and cols variables.*/
+		pixelArray = new Pixel[w/this.rows][h/this.cols];
 		
-		
-		//3. Iterate through the array and initialize each element to a new pixel.
-		
+		/**3. Iterate through the array and initialize each element to a new pixel.*/
+		for(int i = 0; i < this.rows; i++) {
+			for(int j = 0; j < this.cols; j++) {
+				pixelArray[i][j] = new Pixel(i, j);
+			}
+		}
 		
 	}
 	
@@ -46,15 +51,21 @@ public class GridPanel extends JPanel{
 	}
 	
 	public void clickPixel(int mouseX, int mouseY) {
-		//5. Use the mouseX and mouseY variables to change the color
-		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		pixelArray[mouseX/rows][mouseY/cols].color = this.color;
+		/**5. Use the mouseX and mouseY variables to change the color
+		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.*/
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
-		//4. Iterate through the array.
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				
+			}
+		}
+		/*4. Iterate through the array.
 		//   For every pixel in the list, fill in a rectangle using the pixel's color.
-		//   Then, use drawRect to add a grid pattern to your display.
+		//   Then, use drawRect to add a grid pattern to your display.*/
 		
 	}
 }
