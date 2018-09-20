95d9511b44d8711d022cc30d3f9c0067ecb75709
GeneralOy <league@WorthyMini-5.attlocal.net>
2018-09-20 16:57:52 -0700

    asdf
    work on maze

--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -2,8 +2,8 @@ package _04_Maze_Maker;
 import java.awt.Graphics;
 
 public class Maze {
-	//1. Create a 2D array of cells. Don't initialize it.
-	Cell[] CellsArray;
+	/**1. Create a 2D array of cells. Don't initialize it.*/
+	Cell[][]CellsArray;
 	
 	private int width;
 	private int height;
@@ -12,20 +12,30 @@ public class Maze {
 		this.width = w;
 		this.height = h;
 
-		//2. Initialize the cells using the width and height variables
-		
-		//3. Iterate through each cell and initialize it
-		//   using i and j as the location
+		/**2. Initialize the cells using the width and height variables*/
+		CellsArray = new Cell[w][h];
+		/**3. Iterate through each cell and initialize it
+		//   using i and j as the location*/
+		for(int i = 0; i < w; i++) {
+			for(int j = 0; j < h; j++) {
+				CellsArray[i][j] = new Cell(i, j);
+			}
+		}
 		
 	}
 
-	//4. This method iterates through the cells and draws them
+	/**4. This method iterates through the cells and draws them*/
 	public void draw(Graphics g) {
-		
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
+				g.drawRect(i*(MazeDisplay.WIDTH/width), j*(MazeDisplay.HEIGHT/height), MazeDisplay.WIDTH/width, MazeDisplay.HEIGHT/height);
+			}
+		}
 	}
 	
-	//4b. This method returns the selected cell.
+	/*4b. This method returns the selected cell.*/
 	public Cell getCell(int x, int y){
+		
 		return  null;
 	}
 
